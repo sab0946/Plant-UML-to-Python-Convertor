8cef304e78aac3b2a03e7e7e127054361d82fa20
sab0946 <48261602+sab0946@users.noreply.github.com>
2019-04-10 10:54:49 +1200

    Add files via upload
    removed unused attributes

--- module_builder/class_builder.py
+++ module_builder/class_builder.py
@@ -11,50 +11,44 @@ class ClassBuilder:
 
     def __init__(self):
         self.name = ""
-        self.attributes = ""
-        self.methods = ""
-        self.relationships = []
         self.all_my_attributes = []
         self.all_my_methods = []
         self.all_my_parent_classes = []
         self.all_my_composite_classes = []
         self.all_my_associated_classes = []
 
-    def add_class_attributes(self):
-        for an_attribute in self.attributes:
+    def add_class_attributes(self, new_attributes):
+        for an_attribute in new_attributes:
             new_a = Attribute(an_attribute.split(":")[0],
                               an_attribute.split(":")[1])
             self.all_my_attributes.append(new_a)
 
-    def add_class_methods(self):
-        for a_method in self.methods:
+    def add_class_methods(self, new_methods):
+        for a_method in new_methods:
             new_m = Method(a_method.split("(")[0],
                            a_method.split(")")[1],
                            a_method[a_method.find("(")
                                     + 1:a_method.find(")")])
             self.all_my_methods.append(new_m)
 
-    def add_relationships(self):
-        for a_relationship in self.relationships:
+    def add_relationships(self, new_relationships):
+        for a_relationship in new_relationships:
             if "extends" in a_relationship:
                 new_relationship = Relationship(a_relationship)
                 self.all_my_parent_classes.append(new_relationship)
-            if "comp" in a_relationship:
+            elif "comp" in a_relationship:
                 new_relationship = Relationship(a_relationship)
                 self.all_my_composite_classes.append(new_relationship)
-            if "assos" in a_relationship:
+            elif "assos" in a_relationship:
                 new_relationship = Relationship(a_relationship)
                 self.all_my_associated_classes.append(new_relationship)
 
     def build_class(
             self, new_name, new_attributes, new_methods, new_relationships):
         self.name = new_name
-        self.attributes = new_attributes
-        self.methods = new_methods
-        self.relationships = new_relationships
-        self.add_class_attributes()
-        self.add_class_methods()
-        self.add_relationships()
+        self.add_class_attributes(new_attributes)
+        self.add_class_methods(new_methods)
+        self.add_relationships(new_relationships)
 
     def print_class(self):
         string = ""
