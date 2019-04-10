a01861d10e48d6ae2253e4a9f47b46a7e26b16ad
sab0946 <48261602+sab0946@users.noreply.github.com>
2019-04-10 16:28:57 +1200

    Add files via upload
    removed switch statement in the string method

--- module_builder/attribute.py
+++ module_builder/attribute.py
@@ -1,21 +1,15 @@
 from .type_finder import TypeFinder
 
-class Attribute:
 
-    """define the attributes of a class"""
+class Attribute:
 
     def __init__(self, new_name, new_type):
         self.name = new_name.replace(" ", "")
         self.type = TypeFinder.find_type(new_type)
 
     def __str__(self):
-        if "str" in self.type:
-            return f"        self.{self.name}: {self.type} = \"\"  # ToDo\n"
-        elif "int" in self.type:
-            return f"        self.{self.name}: {self.type} = 1  # ToDo\n"
-        elif "list" in self.type:
-            return f"        self.{self.name}: {self.type} = []  # ToDo\n"
-        elif "tuple" in self.type:
-            return f"        self.{self.name}: {self.type} = ()  # ToDo\n"
-        else:
-            return f"        self.{self.name} = None  # ToDo\n"
+        string_dict = {"str": f"        self.{self.name}: {self.type} = \"\"  # ToDo\n",
+                       "int": f"        self.{self.name}: {self.type} = 1  # ToDo\n",
+                       "list": f"        self.{self.name}: {self.type} = []  # ToDo\n",
+                       }
+        return string_dict.get(self.type, f"        self.{self.name} = None  # ToDo\n")
